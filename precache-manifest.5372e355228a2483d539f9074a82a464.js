42f944c967b83f2547a61776360dc1c28db528c1
Thao <tptmac7@gmail.com>
2020-03-01 01:19:06 -0500

    Updates

---
+++ precache-manifest.5372e355228a2483d539f9074a82a464.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2680fff30bd39482be346777afe2e72e",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "dfc68d9edde7ded7c6cf",
+    "url": "/portfolio/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "4ab4bc5e305f5b9c005b",
+    "url": "/portfolio/static/js/2.09f2d3b1.chunk.js"
+  },
+  {
+    "revision": "aa762385ab423a29206547471fcc4bac",
+    "url": "/portfolio/static/js/2.09f2d3b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfc68d9edde7ded7c6cf",
+    "url": "/portfolio/static/js/main.b00a425f.chunk.js"
+  },
+  {
+    "revision": "1a95985286676740b556",
+    "url": "/portfolio/static/js/runtime-main.f6eb50f0.js"
+  }
+]);
